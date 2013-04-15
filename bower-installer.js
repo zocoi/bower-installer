fe53b0ca078e07067bb180220a6914eb59dd6bd6
Bret Little <bret.little@gmail.com>
2013-04-15 16:14:37 -0600

    Change multi path install to be based upon file type

--- bower-installer.js
+++ bower-installer.js
@@ -13,9 +13,9 @@ if(!cfg || !cfg.path) {
     console.log(("bower-installer error").red + " component.json must contain a valid install path");
 }
 
-var installPaths = _.isArray(cfg.path) ? cfg.path : [cfg.path];
+var paths = _.isString(cfg.path) ? {all: cfg.path} : cfg.path;
 
-var installPathFiles =  _.map( installPaths, 
+var installPathFiles =  _.map( paths, 
     function(path) {
         return new File(basePath + '/' + path);
     });   
@@ -28,55 +28,55 @@ var installDependency = function(deps, key) {
         deps = [ deps ];
     }
 
-    _.each(deps, function(dep) {
+    _.each(deps, function(dep) {        
 
-        _.each(installPaths, function(path) {
+        var f_s = dep;
+        var f_name = basePath + '/' + f_s;
+        var f = new File( f_name );      
+        // If the configured paths is a map, use the path for the given file extension  
+        var path = paths.all ? paths.all : paths[f.getExtension()];        
+        var f_path = basePath + '/' + path + '/' + f.getName();
 
-            var f_s = dep;
-            var f_name = basePath + '/' + f_s;
-            var f = new File( f_name );
-            var f_path = basePath + '/' + path + '/' + f.getName();
+        // If it is a directory lets try to read from package.json file
+        if( fs.lstatSync( f_name ).isDirectory() ) {
 
-            // If it is a directory lets try to read from package.json file
-            if( fs.lstatSync( f_name ).isDirectory() ) {
+            var packagejson = f_name + '/' + "package.json";
 
-                var packagejson = f_name + '/' + "package.json";
+            // we want the build to continue as default if case something fails
+            try {
+                // read package.json file
+                var file = fs.readFileSync(packagejson).toString('ascii')
 
-                // we want the build to continue as default if case something fails
-                try {
-                    // read package.json file
-                    var file = fs.readFileSync(packagejson).toString('ascii')
+                // parse file
+                var filedata = JSON.parse(file);
 
-                    // parse file
-                    var filedata = JSON.parse(file);
+                // path to file from main property inside package.json
+                var mainpath = f_name + '/' + filedata.main;
 
-                    // path to file from main property inside package.json
-                    var mainpath = f_name + '/' + filedata.main;
+                // if we have a file reference on package.json to main property and it is a file
+                if( fs.lstatSync( mainpath ).isFile() ) {
 
-                    // if we have a file reference on package.json to main property and it is a file
-                    if( fs.lstatSync( mainpath ).isFile() ) {
-
-                        f = new File( mainpath );
-                        f_path = basePath + '/' + path + '/' + filedata.main;
-                    }
-
+                    f = new File( mainpath );
+                    // Update the output path with the correct file extension
+                    path = paths.all ? paths.all : paths[f.getExtension()];  
+                    f_path = basePath + '/' + path + '/' + filedata.main;
                 }
-                catch( error ) {
-                    // We wont need to show log error, if package.json doesnt exist default to download folder
-                    // console.log(error);
-                }
+
             }
+            catch( error ) {
+                // We wont need to show log error, if package.json doesnt exist default to download folder
+                // console.log(error);
+            }
+        }
 
-            f.copy( f_path, function(error, copied) {
-                if(!error && copied) {
-                    console.log(('\t' + key + ' : ' + f_path).green);
-                } else {
-                    console.log(('Error\t' + dep + ' : ' + f_path).red); 
-                    console.log('\t\t' + error);
-                }
-            });
-
-        });
+        f.copy( f_path, function(error, copied) {
+            if(!error && copied) {
+                console.log(('\t' + key + ' : ' + f_path).green);
+            } else {
+                console.log(('Error\t' + dep + ' : ' + f_path).red); 
+                console.log('\t\t' + error);
+            }
+        });        
 
     });
 };
@@ -106,4 +106,3 @@ bower.commands
     });
 
   });
-
